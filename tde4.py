f43db87169f0fbde3bd9b6fd106e3f73f8211332
xiayoujie96 <zhixingheyi96@gmail.com>
2024-08-20 08:01:51 -0300

    Update tde4.py

--- tde4.py
+++ tde4.py
@@ -1,1 +1,4 @@
 
+'''
+settar o print pra explicar a brincadeira, e definir a escolha se vai ser hxh, hxm, mxm
+pra fazer o humano x maquina e maquina x maquina, da pra usar randint, que vai fazer eles escolherem um numero aleatorio de 1 a 3
