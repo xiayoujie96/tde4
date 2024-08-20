4422f4d0e226ae26873e8d92f1037c5e7b9e1387
xiayoujie96 <zhixingheyi96@gmail.com>
2024-08-20 08:03:10 -0300

    Update tde4.py

--- tde4.py
+++ tde4.py
@@ -2,3 +2,6 @@
 '''
 settar o print pra explicar a brincadeira, e definir a escolha se vai ser hxh, hxm, mxm
 pra fazer o humano x maquina e maquina x maquina, da pra usar randint, que vai fazer eles escolherem um numero aleatorio de 1 a 3
+
+pro jogo ter infinitas rodadas, vamos ter que settar um função com while True: etc etc, e um elif input = 'parar': break, else: raise value error
+algo assim^
