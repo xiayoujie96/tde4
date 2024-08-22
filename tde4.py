1da1a5852eb7c08ae40c4d194f71b98d51aa0bd7
xiayoujie96 <zhixingheyi96@gmail.com>
2024-08-22 11:36:21 -0300

    Update tde4.py

--- tde4.py
+++ tde4.py
@@ -1,14 +1,3 @@
-
-'''
-settar o print pra explicar a brincadeira, e definir a escolha se vai ser hxh, hxm, mxm
-pra fazer o humano x maquina e maquina x maquina, da pra usar randint, que vai fazer eles escolherem um numero aleatorio de 1 a 3
-
-pro jogo ter infinitas rodadas, vamos ter que settar um função com while True: etc etc, e um elif input = 'parar': break, else: raise value error
-algo assim^
-eh importante a gente ter como esconder a escolha da primeira pessoa. acho que da pra fazer um
-escolha1 = int(input('escolha))
-print(\n*10)
-'''
 import random
 
 print("bem vindo, jogadores\n\nmodos de jogo")
@@ -18,16 +7,23 @@ print('(3)Computador x computador')
 total1 = 0
 total2 = 0
 totalempate = 0
+escolha_modo = 0
 nome1 = ''
 nome2 = ''
-def hxh(total1, total2, totalempate, nome1, nome2):
+def atualizar(total1, total2, totalempate, nome1, nome2, escolha_modo):
     while True:
-        jogador1 = int(input(f'Insira sua escolha, {nome1}: '))
-        if jogador1 not in range(1, 4): 
-            print('Você escolheu uma opção inválida. Tente de novo')
+        if escolha_modo == 3:
+            jogador1 = random.randint(1, 3)
+            jogador2 = random.randint(1, 3)
+        if escolha_modo == 2:
+            jogador2 = random.randint(1, 3)
+            jogador1 = int(input(f'Insira sua escolha, {nome1}: '))
+        else: 
+            if jogador1 not in range(1, 4): 
+                print('Você escolheu uma opção inválida. Tente de novo')
             continue
         print('\n'*10)
-        jogador2 = int(input(f'Insira sua escolha, {nome2} '))
+        jogador2 = int(input(f'Insira sua escolha, {nome2}:  '))
         if jogador2 not in range(1, 4): 
             print('Você escolheu uma opção inválida. Tente de novo')
             continue
@@ -42,44 +38,15 @@ def hxh(total1, total2, totalempate, nome1, nome2):
             total2 += 1
         if parar(total1, total2, totalempate, nome1, nome2) == False:
             break
+    
+def hxh(total1, total2, totalempate, nome1, nome2):
+        atualizar(total1, total2, totalempate, nome1, nome2, escolha_modo)
         
 def hxm(total1, total2, totalempate, nome1, nome2): 
-    while True:
-        jogador1 = int(input('Insira sua escolha, {nome1}: '))
-        if jogador1 not in range(1, 4): 
-            print('Você escolheu uma opção inválida. Tente de novo')
-            continue
-        jogador2 = random.randint(1, 3)
-        print(f'O jogador 2 escolheu o numero: {jogador2}')
-        if jogador1 == jogador2:
-            print('Vocês empataram')
-            totalempate += 1
-        elif (jogador1 == 1 and jogador2 == 3) or (jogador1 == 2 and jogador2 == 1) or (jogador1 == 3 and jogador2 == 2):
-            print('O {nome1} ganhou')
-            total1 += 1
-        else: 
-            print('O jogador 2 ganhou')
-            total2 += 1
-        if parar(total1, total2, totalempate, nome1, nome2) == False:
-            break
+        atualizar(total1, total2, totalempate, nome1, nome2, escolha_modo)
 
 def cxc(total1, total2, totalempate, nome1, nome2):
-    while True:
-        jogador1 = random.randint(1, 3)
-        jogador2 = random.randint(1, 3)
-        print(f'O {nome1} escolheu o numero: {jogador1}')
-        print(f'O {nome2} escolheu o numero: {jogador2}')
-        if jogador1 == jogador2:
-            print('Vocês empataram')
-            totalempate += 1
-        elif (jogador1 == 1 and jogador2 == 3) or (jogador1 == 2 and jogador2 == 1) or (jogador1 == 3 and jogador2 == 2):
-            print(f'O {nome1} ganhou')
-            total1 += 1
-        else: 
-            print(f'O {nome2} ganhou')
-            total2 += 1
-        if parar(total1, total2, totalempate, nome1, nome2) == False:
-            break
+        atualizar(total1, total2, totalempate, nome1, nome2, escolha_modo)
         
 def parar(total1, total2, totalempate, nome1, nome2):
     while True:
@@ -100,7 +67,7 @@ def parar(total1, total2, totalempate, nome1, nome2):
         else:
             print('Opção inválida. Tente novamente')     
                                       
-def modo(nome1, nome2):
+def modo(nome1, nome2, escolha_modo):
     escolha_modo = int(input('Escolha o modo: '))
     print('As escolhas são: ')
     print('Pedra(1)')
@@ -120,6 +87,5 @@ def modo(nome1, nome2):
         cxc(total1, total2, totalempate, nome1, nome2)
     else:
         print('Escolha inválida. Tente novamente')
-        return modo(nome1, nome2)  
-    
-modo(nome1, nome2)
+        return modo(nome1, nome2, escolha_modo)  
+modo(nome1, nome2, escolha_modo)
