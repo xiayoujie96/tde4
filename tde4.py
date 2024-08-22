bad27b0630d160e60bc16989e0af30ef5612976b
xiayoujie96 <zhixingheyi96@gmail.com>
2024-08-22 10:35:43 -0300

    Update tde4.py

--- tde4.py
+++ tde4.py
@@ -1,14 +1,3 @@
-
-'''
-settar o print pra explicar a brincadeira, e definir a escolha se vai ser hxh, hxm, mxm
-pra fazer o humano x maquina e maquina x maquina, da pra usar randint, que vai fazer eles escolherem um numero aleatorio de 1 a 3
-
-pro jogo ter infinitas rodadas, vamos ter que settar um função com while True: etc etc, e um elif input = 'parar': break, else: raise value error
-algo assim^
-eh importante a gente ter como esconder a escolha da primeira pessoa. acho que da pra fazer um
-escolha1 = int(input('escolha))
-print(\n*10)
-'''
 import random
 
 print("bem vindo, jogador\n\nmodos de jogo")
